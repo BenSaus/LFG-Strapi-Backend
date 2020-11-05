b99a1946fa0edd61a3c3c89defe97d84e17995c5
BenSaus <jonas26a@gmail.com>
2020-11-04 18:00:10 -0700

    Cors config

--- config/middleware.js
+++ config/middleware.js
@@ -1,11 +1,23 @@
 module.exports = ({ env }) => ({
-  //   settings: {
-  //     cors: {
-  //       origin: [
-  //         "http://localhost:3000",
-  //         "https://lfg-react-frontend-k88vi.ondigitalocean.app",
-  //         "http://167.71.126.201",
-  //       ],
-  //     },
-  //   },
+  settings: {
+    cors: {
+      enabled: true,
+      origin: "*",
+      expose: [
+        "WWW-Authenticate",
+        "Server-Authorization",
+        "Access-Control-Expose-Headers",
+      ],
+      maxAge: 31536000,
+      credentials: true,
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
+      headers: [
+        "Content-Type",
+        "Authorization",
+        "X-Frame-Options",
+        "Origin",
+        "Access-Control-Allow-Headers",
+      ],
+    },
+  },
 });
