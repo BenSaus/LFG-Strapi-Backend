660ed583a40543da5c6a69c926ef77e9406c731c
BenSaus <jonas26a@gmail.com>
2020-11-04 17:10:32 -0700

    More cors config changes

--- config/middleware.js
+++ config/middleware.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   settings: {
     cors: {
-      enabled: false,
       origin: [
         "http://localhost:3000",
         "https://lfg-react-frontend-k88vi.ondigitalocean.app",
