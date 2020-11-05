f40c385b17bbd8b7f3b150f1a3e9cc4e187c1057
BenSaus <jonas26a@gmail.com>
2020-11-04 17:30:03 -0700

    Forgot to add admin into cors origins

--- config/middleware.js
+++ config/middleware.js
@@ -4,6 +4,7 @@ module.exports = ({ env }) => ({
       origin: [
         "http://localhost:3000",
         "https://lfg-react-frontend-k88vi.ondigitalocean.app",
+        "http://167.71.126.201",
       ],
     },
   },
