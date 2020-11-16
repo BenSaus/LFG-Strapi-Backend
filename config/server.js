ee3b4b2f752ded57bdc877cbd81a2b45bf7bc0ca
BenSaus <jonas26a@gmail.com>
2020-11-16 13:47:24 -0700

    Model changes to user,invite,and group

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'deb3b6059cb2a6e17194acc4c2c4af13'),
+      secret: env("ADMIN_JWT_SECRET", "deb3b6059cb2a6e17194acc4c2c4af13"),
     },
   },
 });
