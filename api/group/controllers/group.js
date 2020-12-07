c1d633b6cb70f7a56bb14fda2c860e5a9c1f5731
BenSaus <jonas26a@gmail.com>
2020-12-07 13:03:23 -0700

    Adding create group custom endpoint

--- api/group/controllers/group.js
+++ api/group/controllers/group.js
@@ -1,8 +1,43 @@
 "use strict";
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+class GroupMaximumError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "GroupMaximumError";
+    this.code = "1000";
+  }
+}
+
+// TODO: Move this into const file
+const MAX_GROUP_NUMBER = 3;
+
+module.exports = {
+  async create(ctx) {
+    let entity;
+
+    console.log("Create group called!!! by " + ctx.state.user.id);
+
+    const leadGroups = await strapi.services.group.find({
+      leader: ctx.state.user.id,
+    });
+    const groupCount = leadGroups.length;
+    console.log(groupCount);
+
+    if (groupCount < MAX_GROUP_NUMBER) {
+      entity = await strapi.services.group.create(ctx.request.body);
+    } else {
+      // TODO: A descriptive error should be returned here...
+      throw new GroupMaximumError(
+        "You have already created the maximum number of groups"
+      );
+    }
+
+    return sanitizeEntity(entity, { model: strapi.models.group });
+  },
+};
