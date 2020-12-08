08e62b9528bcfc17ca6100a057edbc16f029bc07
BenSaus <jonas26a@gmail.com>
2020-12-08 14:43:17 -0700

    Set up endpoint testing

--- api/group/controllers/group.js
+++ api/group/controllers/group.js
@@ -18,26 +18,22 @@ class GroupMaximumError extends Error {
 const MAX_GROUP_NUMBER = 3;
 
 module.exports = {
-  async create(ctx) {
-    let entity;
-
-    console.log("Create group called!!! by " + ctx.state.user.id);
-
-    const leadGroups = await strapi.services.group.find({
-      leader: ctx.state.user.id,
-    });
-    const groupCount = leadGroups.length;
-    console.log(groupCount);
-
-    if (groupCount < MAX_GROUP_NUMBER) {
-      entity = await strapi.services.group.create(ctx.request.body);
-    } else {
-      // TODO: A descriptive error should be returned here...
-      throw new GroupMaximumError(
-        "You have already created the maximum number of groups"
-      );
-    }
-
-    return sanitizeEntity(entity, { model: strapi.models.group });
-  },
+  //   async create(ctx) {
+  //     let entity;
+  //     console.log("Create group called!!! by " + ctx.state.user.id);
+  //     const leadGroups = await strapi.services.group.find({
+  //       leader: ctx.state.user.id,
+  //     });
+  //     const groupCount = leadGroups.length;
+  //     console.log(groupCount);
+  //     if (groupCount < MAX_GROUP_NUMBER) {
+  //       entity = await strapi.services.group.create(ctx.request.body);
+  //     } else {
+  //       // TODO: A descriptive error should be returned here...
+  //       throw new GroupMaximumError(
+  //         "You have already created the maximum number of groups"
+  //       );
+  //     }
+  //     return sanitizeEntity(entity, { model: strapi.models.group });
+  //   },
 };
---
+++ tests/app.test.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const { setupStrapi } = require("./helpers/strapi");
+const { createTestUser, setupPermissions } = require("./helpers/strapi-user");
+
+beforeAll(async (done) => {
+  await setupStrapi();
+  //   await createTestUser();
+  await setupPermissions();
+  done();
+});
+
+it("strapi is defined", () => {
+  expect(strapi).toBeDefined();
+});
+
+require("./user");
+require("./group");
